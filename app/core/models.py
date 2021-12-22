47e3e604109f3b8e11cfe579a9623a1b20b5ad58
Xiaoyu.Liu <liuxy199803@163.com>
2021-12-21 19:28:40 -0600

    Uer API

--- app/core/models.py
+++ app/core/models.py
@@ -10,8 +10,8 @@ class UserManager(BaseUserManager):
         user=self.model(email=self.normalize_email(email),**extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
+        
     def create_superuser(self,email,password):
         user=self.create_user(email,password)
         user.is_staff=True
